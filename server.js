20b04d3d234c0a93d2ef1abf75d2157c7398c358
Everton Azevedo <everton.azevedo@confitec.com.br>
2022-10-17 19:06:57 -0300

    REC-15 - Validar Login

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+//Constantes
 const express = require('express');
 const app = express();
 const path = require('path');
 const router = express.Router();
 //const cookieParser = require('cooki-parser');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
+//Importação das Tabelas
 const tabelaEstado = require('./src/models/estado');
 const tabelaCidade = require('./src/models/cidade');
 const tabelaEndereco = require('./src/models/endereco');
@@ -14,6 +17,15 @@ const tabelaUsuario = require('./src/models/usuario');
 const tabelaMaterial = require('./src/models/material');
 const tabelaColeta = require('./src/models/coleta');
 
+//Importação das rotas
+const rotasUsuario = require('./src/routes/rotasUsuario');
+
+//Configurações
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, "/")));
+
 router.get('/', function (req, res) {
 
     res.sendFile(path.join(__dirname + '/index.html'));
@@ -21,9 +33,9 @@ router.get('/', function (req, res) {
 });
 
 app.use('/', router);
+app.use('/usuario', rotasUsuario);
 app.use(cors());
 //app.use(cookieParser());
 app.use(express.json());
-app.listen(process.env.port || 3000);
-
+app.listen(process.env.port || 5500);
 
