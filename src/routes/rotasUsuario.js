9a43333664b749212b6d1d3156606a407c5ff3af
Everton Azevedo <everton.azevedo@confitec.com.br>
2022-10-17 09:23:33 -0300

    REC-15 - Login com o google

---
+++ src/routes/rotasUsuario.js
@@ -0,0 +1,24 @@
+/*const express = require('express');
+const router = express.Router();
+
+/*Login Google
+router.post('/usuario-google', function(req, res) {
+
+    const data = jwt_decode(res.credential);
+
+    tabelaUsuario.create({
+
+        email: data.email,
+        senha: data.sub,
+        nm_usuario: data.given_name,
+        sobrenome_usuario: data.family_name
+
+    }).then(function () {
+        console.log('Usuário salvo com sucesso! Usuário: ' + usuario);
+    }).catch(function (error) {
+        console.log('Erro ao salvar usuário: ' + error);
+    });
+
+});
+
+module.exports = router;*/
