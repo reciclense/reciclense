7454b5aea547993e207cd168c3d0a422484ecc99
Vinicius Lima <viniciusmateus.dev@gmail.com>
2022-11-01 16:42:13 -0300

    Alterado os arquivos rotaUsuario.js e pessoaJuridica.js

--- src/assets/js/pessoaJuridica.js
+++ src/assets/js/pessoaJuridica.js
@@ -1,97 +1,105 @@
-//Dados para popular a tabela
-const dados = [ ['Av Caxangá', 'Recife', 'Pernambuco', 'Metal', 'Everton'],
-                ['Av Caxangá', 'Recife', 'Pernambuco', 'Papel', 'Magnum'],
-                ['Av Caxangá', 'Recife', 'Pernambuco', 'Plástico', 'Vinicius']
-]
+// Recuperando o JSON das coletas através do método GET
+const options = { method: 'GET', headers: { 'Content-Type': 'application/json' } };
 
-function criarTag(elemento) {
-    return document.createElement(elemento);
-}
+fetch('http://localhost:5500/listar-coletas', options)
+    .then(response => response.json())
+    .then(response => {
 
-let tabela = document.getElementById("tbl-pesquisa");
+        // Dados para popular a tabela
 
-let thead = criarTag("thead");
-let tbody = criarTag("tbody");
-let tfoot = criarTag("tfoot");
+        const dados = [];
 
-let indicesTabela = ["Endereço", "Cidade", "Estado", "Material", "Solicitante"];
-let linhaHead = criarTag("tr");
+        // Percorrendo o objeto response.dados e atribuindo um array dentro do array dados
+        for (let i = 0; i < response.dados.length; i++) {
+            dados[i] = [response.dados[i].usuario.endereco.nm_logradouro, response.dados[i].usuario.endereco.numero, response.dados[i].usuario.endereco.nm_bairro,
+            response.dados[i].usuario.endereco.cidade.nm_cidade, response.dados[i].usuario.endereco.cidade.estado.sigla_uf,
+            response.dados[i].usuario.endereco.nm_complemento, response.dados[i].data, response.dados[i].horario,
+            response.dados[i].usuario.nm_usuario, response.dados[i].observacao]
 
-function criarCelula(tag, text) {
-    tag = criarTag(tag);
-    tag.textContent = text;
-    return tag;
-}
+        }
 
-for(let j = 0; j < indicesTabela.length; j++){
-    let th = criarCelula("th", indicesTabela[j]);
-    linhaHead.appendChild(th);
-}
-thead.appendChild(linhaHead);
+        // Função para criar uma Tag Ex: <tr>, <td>
+        function criarTag(elemento) {
+            return document.createElement(elemento);
+        }
 
-for (let j = 0, linhaBody = ''; j < dados.length; j++) {
-    //console.log(dados[j]);
-    linhaBody = criarTag("tr");
-    if ( j % 2 != 0) {
-        linhaBody.setAttribute("class", "table-active");
-    }
-    for(let i = 0, cel = ''; i < dados[j].length; i++){
-        //console.log(dados[j][i]);
-        cel = criarCelula("td", dados[j][i]);
-        linhaBody.appendChild(cel);
-    }
-    tbody.appendChild(linhaBody);
-}
+        // Criando a tabela
+        let tabela = document.getElementById("tbl-pesquisa");
+        let thead = criarTag("thead");
+        let tbody = criarTag("tbody");
+        let tfoot = criarTag("tfoot");
 
-tabela.appendChild(thead);
-tabela.appendChild(tbody);
-tabela.appendChild(tfoot);
+        let indicesTabela = ["Rua", "Numero", "Bairro", "Cidade", "Estado", "Complemento", "Data", "Horario", "Solicitante", "Observação"];
+        let linhaHead = criarTag("tr");
 
+        // Função para criar uma celular Ex <th> + o texto
+        function criarCelula(tag, text) {
+            tag = criarTag(tag);
+            tag.textContent = text;
+            return tag;
+        }
 
+        // Percorrendo o indice das tabelas e atribuindo a tag <thead>
+        for (let j = 0; j < indicesTabela.length; j++) {
+            let th = criarCelula("th", indicesTabela[j]);
+            linhaHead.appendChild(th);
+        }
+        thead.appendChild(linhaHead);
 
+        // Percorrendo os dados e atribuindo as colunas e adicionando em cada linha
+        for (let j = 0, linhaBody = ''; j < dados.length; j++) {
+            //console.log(dados[j]);
+            linhaBody = criarTag("tr");
+            if (j % 2 != 0) {
+                linhaBody.setAttribute("class", "table-active");
+            }
+            for (let i = 0, cel = ''; i < dados[j].length; i++) {
+                //console.log(dados[j][i]);
+                cel = criarCelula("td", dados[j][i]);
+                linhaBody.appendChild(cel);
+            }
+            tbody.appendChild(linhaBody);
+        }
 
-/*
-//Popular tabela
-let tbody = document.getElementById("tbody");
-for (let i = 0; i < dados.length; i++) {
-    let tr = "<tr>" +
-    "<td>" + dados[i][0] + "</td>" +
-    "<td>" + dados[i][1] + "</td>" +
-    "<td>" + dados[i][2] + "</td>" +
-    "<td>" + dados[i][3] + "</td>" +
-    "<td>" + dados[i][4] + "</td>" +
-    "</tr>";
-    tbody.innerHTML += tr;
+        // Atribuindo as tags da tabela na tag <table>
+        tabela.appendChild(thead);
+        tabela.appendChild(tbody);
+        tabela.appendChild(tfoot);
 
-}
+    })
+    .catch(err => console.error(err));
 
-//Barra de pesquisa
+// Pesquisa dinâmica
 document.getElementById("ipt-pesquisa").addEventListener("keyup", function () {
-    
+
+    let tbody = document.querySelector('tbody')
     let busca = document.getElementById("ipt-pesquisa").value.toLowerCase();
 
-    for (let i = 1; i < tbody.childNodes.length; i++) { 
+    // Percorrendo as do body para encontrar um valor
+    for (let i = 0; i < tbody.childNodes.length; i++) {
         console.log(tbody.childNodes)
         let achou = false;
         let tr = tbody.childNodes[i];
         let td = tr.childNodes;
-        console.log(tr)
+        console.log(td)
+        // Percorrendo as colunas do body para encontrar um valor
         for (let j = 0; j < td.length; j++) {
             let value = td[j].childNodes[0].nodeValue.toLowerCase();
-
+            console.log(value)
 
             if (value.indexOf(busca) >= 0) {
                 achou = true;
             }
         }
 
+        // Se encontrar, adicionar a classe "table-row" para exibir as linhas da pesquisa
         if (achou) {
             tr.style.display = "table-row";
-        } else {
+        } 
+        // Se não encontrar, adicionar a classe "none" para esconder as linhas
+        else {
             tr.style.display = "none";
         }
     }
 
 });
-
-*/
