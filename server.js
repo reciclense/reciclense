dca4a54fdfd60dbe4f1dfcc2ee82c557beed5e6b
Everton Azevedo <everton.azevedo@confitec.com.br>
2022-11-08 22:02:54 -0300

    Ajuste ornaginação arquivos de requisição front

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const tabelaColeta = require('./src/models/coleta');
 //Importação das rotas
 const rotaValidarLogin = require('./src/routes/validarLogin');
 const rotaListarColetas = require('./src/routes/listarColetas');
-const rotaCadastrarUsuarioGoogle = require('./src/routes/cadastrarUsuarioGoogle');
+const rotaLoginGoogle = require('./src/routes/loginGoogle');
 const rotaCadastrarUsuario = require('./src/routes/cadastrarUsuario');
 const rotaBuscarUsuarioGoogle = require('./src/routes/buscarUsuarioGoogle');
 const rotaBtnDinamico = require('./src/routes/btnDinamico');
@@ -38,7 +38,7 @@ router.get('/', function (req, res) {
 });
 
 app.use('/', router);
-app.use(rotaValidarLogin, rotaListarColetas, rotaCadastrarUsuarioGoogle, rotaCadastrarUsuario, rotaBuscarUsuarioGoogle, rotaBtnDinamico);
+app.use(rotaValidarLogin, rotaListarColetas, rotaLoginGoogle, rotaCadastrarUsuario, rotaBuscarUsuarioGoogle, rotaBtnDinamico);
 app.use(cors());
 //app.use(cookieParser());
 app.use(express.json());
---
+++ src/assets/js/validarLogin.js
@@ -0,0 +1,58 @@
+/*Autenticar Usuário*/
+document.getElementById('btnEntrar')
+    .addEventListener('click', function () {
+
+        //Pegando valores dos campos email e senha do formulário
+        let email = document.getElementById('exampleInputEmail1').value;
+        let senha = document.getElementById('exampleInputPassword1').value;
+
+        //Configuração da rota
+        const options = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                email,
+                senha
+            })
+        };
+
+        //Fetch para redirecionar usuário de acordo com o tp_perfil ou apresentar alert 
+        fetch('http://localhost:5500/valida-login', options)
+            .then(response => response.json())
+            .then(async response => {
+                if (response.success == false) {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: 'Usuário ou senha incorreta!'
+                    });
+                } else {
+                    const Toast = Swal.mixin({
+                        toast: true,
+                        position: 'top-end',
+                        showConfirmButton: false,
+                        timer: 2000,
+                        timerProgressBar: true,
+                        didOpen: (toast) => {
+                            toast.addEventListener('mouseenter', Swal.stopTimer)
+                            toast.addEventListener('mouseleave', Swal.resumeTimer)
+                        }
+                    })
+
+                    await Toast.fire({
+                        icon: 'success',
+                        title: 'Logado com sucesso'
+                    })
+
+                    //Salvando token no localStorage
+                    localStorage.setItem("token", response.token);
+
+                    if (response.tp_perfil == 'fisica') {
+                        window.location.href = "src/pages/pessoaFisicaPrincipal.html";
+                    } else {
+                        window.location.href = "src/pages/pessoaJuridicaPrincipal.html";
+                    }
+                }
+            })
+            .catch(err => console.error(err));
+    });
